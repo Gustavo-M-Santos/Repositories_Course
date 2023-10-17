2301ff97d3cad0d771a5fa533031634f6ad12326
gustavo <gmelo5805@gmail.com>
2023-10-17 16:02:50 -0300

    Multi-account activity and validations

--- Projets/Java/script.js
+++ Projets/Java/script.js
@@ -1,40 +1,40 @@
-
-let index = 1
-const patients = [
-  {
-    name: "Luiz",
-    age: 20,
-    weight: 100,
-    height: 190,
-  },
-  {
-    name: "Alexandra",
-    age: 27,
-    weight: 70,
-    height: 170,
-  },
-  {
-    name: "Carlos",
-    age: 42,
-    weight: 90,
-    height: 180,
-  },
-]
-
-let patientsNames = []
-let patientWeight = []
-
-function calcIMC(weight, height) {
-  return ((weight) / ((height / 100)**2)).toFixed(2)
-}
+let numberOne = Number(prompt('Qual é o primeiro numero?'))
+let numberTwo = Number(prompt('Qual é o segundo numero?'))
 
 
-for(let patient of patients) {
-  patientsNames.push(alert(`Paciente ${index++}:
-  Nome: ${patient.name}
-  Idade: ${patient.age}
-  Peso: ${patient.weight} kg
-  Altura: ${patient.height} cm
-  IMC: ${calcIMC(patient.weight, patient.height)}`))
+  function contMats(numberOne, numberTwo) {
 
-}
+    let resultCont1 = numberOne + numberTwo
+    let resultCont2 = numberOne - numberTwo
+    let resultCont3 = numberOne * numberTwo
+    let resultCont4 = numberOne / numberTwo
+    let resultCont5 = numberOne % numberTwo
+
+    if (numberOne == numberTwo) {
+      alert('Os números fornecidos são iguais.')
+    } else {
+      alert('Os números fornecidos são diferentes.')
+    }
+
+    function confNumberOddPair(resulteCont) {
+       if ((resulteCont % 2) == 0) {
+        return 'par'
+      } else {
+        return 'impar'
+      }
+    }
+
+    alert(`
+      O resultado da conta é: ${resultCont1}. Sendo número o resultado ${confNumberOddPair(resultCont1)}
+
+      O resultado da conta é: ${resultCont2}. Sendo número o resultado ${confNumberOddPair(resultCont2)}
+
+      O resultado da conta é: ${resultCont3}. Sendo número o resultado ${confNumberOddPair(resultCont3)}
+
+      O resultado da conta é: ${resultCont4}. Sendo número o resultado ${confNumberOddPair(resultCont4)}
+
+      O resultado da conta é: ${resultCont5}. Sendo número o resultado ${confNumberOddPair(resultCont5)}
+    `)
+  }
+
+  contMats(numberOne, numberTwo)
