42f5ba7103c61eaae5cb795d126235df48dc0ab9
gustavo <gmelo5805@gmail.com>
2023-10-18 10:30:30 -0300

    List Approv and Disapprov people calc average

--- Projets/Java/script.js
+++ Projets/Java/script.js
@@ -1,40 +1,97 @@
-let numberOne = Number(prompt('Qual é o primeiro numero?'))
-let numberTwo = Number(prompt('Qual é o segundo numero?'))
+let optionMenu = 0
+const students = [
 
+]
 
-  function contMats(numberOne, numberTwo) {
+while (optionMenu != 3) {
+  optionMenu = Number(prompt(`
+  Escolha uma das opções:
+    1° - Adicionar um aluno.
+    2° - Ver a lista de aprovados e Reprovados.
+    3° - Cancelar operação.
+  `))
 
-    let resultCont1 = numberOne + numberTwo
-    let resultCont2 = numberOne - numberTwo
-    let resultCont3 = numberOne * numberTwo
-    let resultCont4 = numberOne / numberTwo
-    let resultCont5 = numberOne % numberTwo
+  switch (optionMenu) {
+    case 1:
+      const name = prompt('Qual é o nome do Aluno')
+      const sobName = prompt('Qual é o sobre nome do Aluno')
+      const noteBi1 = Number(prompt('Digite a nota do 1° bimestre'))
+      const noteBi2 = Number(prompt('Digite a nota do 2° bimestre'))
+      const noteBi3 = Number(prompt('Digite a nota do 3° bimestre'))
+      const noteBi4 = Number(prompt('Digite a nota do 4° bimestre'))
+      const average = (noteBi1 + noteBi2 + noteBi3 + noteBi4) / 4
+      const passed = average >= 7
 
-    if (numberOne == numberTwo) {
-      alert('Os números fornecidos são iguais.')
-    } else {
-      alert('Os números fornecidos são diferentes.')
-    }
-
-    function confNumberOddPair(resulteCont) {
-       if ((resulteCont % 2) == 0) {
-        return 'par'
-      } else {
-        return 'impar'
+      const student = {
+        name,
+        sobName,
+        noteBi1,
+        noteBi2,
+        noteBi3,
+        noteBi4,
+        average,
+        passed,
       }
-    }
 
-    alert(`
-      O resultado da conta é: ${resultCont1}. Sendo número o resultado ${confNumberOddPair(resultCont1)}
+      students.push(student)
 
-      O resultado da conta é: ${resultCont2}. Sendo número o resultado ${confNumberOddPair(resultCont2)}
+      alert('Aluno adicionado com sucesso!');
+      break;
+    case 2:
+      while (true) {
+        const subMenuOption = Number(prompt(`
+          Escolha uma das opções:
+          1 - Ver lista de aprovados
+          2 - Ver lista de reprovados
+          3 - Retornar ao menu principal
+        `));
 
-      O resultado da conta é: ${resultCont3}. Sendo número o resultado ${confNumberOddPair(resultCont3)}
+        switch (subMenuOption) {
+          case 1:
+            if(students.length == 0){
+              alert('Nenhum aluno(a) encontrado. Por favor adicione alunos(a)')
+            }else{
+              const passedStudents  = students.filter(student => student.passed)
+              for(const student of passedStudents){
+                alert(`${student.name} teve uma média anual de ${student.average}.\n Meus parabéns voce passou de ano!`)
+              }
+            }
+            break;
 
-      O resultado da conta é: ${resultCont4}. Sendo número o resultado ${confNumberOddPair(resultCont4)}
+          case 2:
 
-      O resultado da conta é: ${resultCont5}. Sendo número o resultado ${confNumberOddPair(resultCont5)}
-    `)
+          if(students.length == 0){
+            alert('Nenhum aluno(a) encontrado. Por favor adicione alunos(a)')
+          }else{
+            const failedStudents   = students.filter(student => !student.passed)
+            for(const student of failedStudents){
+              alert(`${student.name} teve uma média anual de ${student.average}.\n Me desculpe mas voce não passou de ano!`)
+            }
+          }
+
+            break;
+
+          case 3:
+            alert('Retornando ao menu principal');
+            break;
+
+          default:
+            alert('Opção inválida');
+            break;
+        }
+
+        if (subMenuOption === 3) {
+          break; // Sai do loop interno e retorna ao menu principal
+        }
+      }
+      break;
+    case 3:
+      alert('operação encerrada')
+      break;
+    default:
+      alert('operação invalida')
+      break;
   }
 
-  contMats(numberOne, numberTwo)
+}
+
