9b5cc2c0d850ef1b45329f5fb4b186a37e4d6f58
gustavo <gmelo5805@gmail.com>
2023-10-31 18:13:06 -0300

    Lógica do countdown

--- Projets/AtivJS/FocusTime/scr/FocusTimer/actions.js
+++ Projets/AtivJS/FocusTime/scr/FocusTimer/actions.js
@@ -10,6 +10,7 @@ export function toggleRunning() {
 export function reset() {
   states.isRunning = false;
   document.documentElement.classList.remove('running');
+  timer.updateDisplay()
 }
 
 export function set() {
